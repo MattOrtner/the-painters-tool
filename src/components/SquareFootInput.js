df9afb966f5d09187350b9198bd0f56158b3f01a
Matt Ortner <matthewortner@gmail.com>
2021-03-17 16:38:50 -0400

    fix spacing on l,w,h inputs

--- src/components/SquareFootInput.js
+++ src/components/SquareFootInput.js
@@ -4,24 +4,26 @@ const SquareFootInput = ({ calculate, handleLHWInputs, setTotalFootage }) => {
 
   return (
     <div className="inputs">
-      <div className="input-pairing">
-        <input className="input" type="number" placeholder="length" name="length" onChange={handleLHWInputs} />
-        <div className="feet">ft.</div>
+      <div className="top-inputs">
+        <div className="input-pairing">
+          <input className="input" type="number" placeholder="length" name="length" onChange={handleLHWInputs} />
+          <div className="feet">ft.</div>
+        </div>
+        <div className="input-pairing">
+          <input type="number" className="input" placeholder="width" name="width" onChange={handleLHWInputs} />
+          <div className="feet">ft.</div>
+        </div>
+        <div className="input-pairing">
+          <input type="number" className="input" placeholder="height" name="height" onChange={handleLHWInputs} />
+          <div className="feet">ft.</div>
+        </div>
       </div>
-      <div className="input-pairing">
-        <input type="number" className="input" placeholder="width" name="width" onChange={handleLHWInputs} />
-        <div className="feet">ft.</div>
-      </div>
-      <div className="input-pairing">
-        <input type="number" className="input" placeholder="height" name="height" onChange={handleLHWInputs} />
-        <div className="feet">ft.</div>
-      </div>
-      <div className="break-in-inputs">OR</div>
+      <div className="or-input-divider">OR</div>
       <div className="input-pairing">
         <input type="number"  className="input" placeholder="Total" name="total" onChange={handleLHWInputs}/>
         <div className="feet">ft.</div>
       </div>
-      <input className="button" type="button" value="CALC SQ. FOOTAGE" onClick={calculate} />
+      <input className="sq-foot-button button" type="button" value="CALC SQ. FOOTAGE" onClick={calculate} />
     </div>
   )
 }
