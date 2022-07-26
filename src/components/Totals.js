36873b062da037256eaa144a1ddb51f4e4803ebc
Matt Ortner <matthewortner@gmail.com>
2022-07-26 17:27:37 -0400

    set width left and right containers. Brought final cost button out and below totals

--- src/components/Totals.js
+++ src/components/Totals.js
@@ -2,17 +2,19 @@ import React from "react";
 
 const Totals = ({ rate, paintCost, totalCost, calcFinal }) => {
   return (
-    <div className="totals">
-      <div>cost of work </div>
-      <div>$ {rate} </div>
-      <div>estimate cost of paint </div>
-      <div>$ {paintCost}</div>
-      <div>total estimate </div>
-      <div>$ {totalCost}</div>
+    <>
+      <div className="totals">
+        <div>cost of work </div>
+        <div>$ {rate} </div>
+        <div>estimate cost of paint </div>
+        <div>$ {paintCost}</div>
+        <div>total estimate </div>
+        <div>$ {totalCost}</div>
+      </div>
       <button className="button" onClick={calcFinal}>
         finalize estimate
       </button>
-    </div>
+    </>
   );
 };
 
